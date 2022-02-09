46c8d401ca4a475fda3ef1a8e8d1efb9d873f792
Veronika <vprikolota92@gmail.com>
2022-02-09 14:56:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = false;
+const isClose = false;
+
+console.log(isChecked || !isClose);
